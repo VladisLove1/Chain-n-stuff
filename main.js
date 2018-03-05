c8020e005c146a987e5b0501d06a47658ec527fc
VladisLove1 <vladislove0212@gmail.com>
2018-03-05 19:02:07 +0200

    error handled

--- main.js
+++ main.js
@@ -5,10 +5,11 @@
 const SHA256 = require('crypto-js/sha256');
 
 class Transaction {
-    constructor(fromAddress, toAddress, amount){
+    constructor(fromAddress, toAddress, amount) {
         this.fromAddress = fromAddress;
         this.toAddress = toAddress;
         this.amount = amount;
+    }
 }
     
 class Block {
